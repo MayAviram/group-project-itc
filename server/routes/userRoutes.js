7f1c60ed5d07c9d8a4fc1b442b439ab748184b6e
Jeki <jekikrespi@gmail.com>
2023-03-22 19:37:20 +0200

    routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,28 +1,17 @@
-<<<<<<< HEAD
 const express = require("express");
-const { createUser, loginUser } = require("../controllers/userController");
-const { validateUser } = require("../middleware/middleware");
-
-const router = express.Router();
-
-router.post("/signup", validateUser, createUser);
-router.post("/login", loginUser);
-=======
-const express = require('express');
 const {
   createUser,
   loginUser,
   getUserById,
   updateUser,
-} = require('../controllers/userController');
-const { validateUser, existUser } = require('../middlewares/userMiddlewares');
+} = require("../controllers/userController");
+const { validateUser, existUser } = require("../middlewares/userMiddlewares");
 
 const router = express.Router();
 
-router.post('/signup', validateUser, createUser);
-router.post('/login', loginUser);
-router.get('/:id', existUser, getUserById);
-router.put('/:id', existUser, updateUser);
->>>>>>> aa053d88d5781a16b926f03ceb38848e9cd82a71
+router.post("/signup", validateUser, createUser);
+router.post("/login", loginUser);
+router.get("/:id", existUser, getUserById);
+router.put("/:id", existUser, updateUser);
 
 module.exports = { userRoutes: router };
