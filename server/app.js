bfa4426d0821ddafd40747fb38568e5f092bf59c
May Aviram <108925345+MayAviram@users.noreply.github.com>
2023-03-23 18:06:15 +0200

    .

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { teacherRoutes } = require('./routes/teacherRoutes');
-const { userRoutes } = require('./routes/userRoutes');
+const express = require("express");
+const { teacherRoutes } = require("./routes/teacherRoutes");
+const { userRoutes } = require("./routes/userRoutes");
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/teachers', teacherRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/teachers", teacherRoutes);
 
 module.exports = { app };
