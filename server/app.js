47eae49af9838a293a1b5c996fa7dd16d6a6b73a
Daniel Macedo <daniel.mabus@me.com>
2023-03-23 13:05:11 +0200

    CORS INSTALED

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
-const express = require("express");
-const { teacherRoutes } = require("./routes/teacherRoutes");
-const { userRoutes } = require("./routes/userRoutes");
+const express = require('express');
+const cors = require('cors');
+const { teacherRoutes } = require('./routes/teacherRoutes');
+const { userRoutes } = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/teachers", teacherRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/teachers', teacherRoutes);
 
 module.exports = { app };
