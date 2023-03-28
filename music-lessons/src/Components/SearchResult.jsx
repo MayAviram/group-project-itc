1a0a11e7e648497dd124bd0c6fca8a7f0e20a6c8
Daniel Macedo <daniel.mabus@me.com>
2023-03-28 13:08:52 +0300

    GET FAVORITE FIXED ON THE CLIENT SITE

--- music-lessons/src/Components/SearchResult.jsx
+++ music-lessons/src/Components/SearchResult.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react";
-import TeacherCard from "./Teachers/TeacherCard";
-import { Line, Column } from "../Layouts/layouts";
-import "./SearchBar.css";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import TeacherCard from './Teachers/TeacherCard';
+import { Line, Column } from '../Layouts/layouts';
+import './SearchBar.css';
+import axios from 'axios';
 
 export default function SearchResult({ teacherList }) {
-  const [filter, setFilter] = useState("Raiting");
+  const [filter, setFilter] = useState('Raiting');
   const [list, setList] = useState();
 
   useEffect(() => {
     if (list) {
       const sortedList = [...list];
       switch (filter) {
-        case "Raiting":
+        case 'Raiting':
           sortedList.sort((a, b) => b.raiting - a.raiting);
 
           break;
-        case "Price":
+        case 'Price':
           sortedList.sort((a, b) => a.price - b.price);
       }
       setList(sortedList);
@@ -34,14 +34,13 @@ export default function SearchResult({ teacherList }) {
       try {
         const response = await axios.get(
           `http://localhost:4006/api/v1/teachers/getfavorites`,
-          localStorage.getItem("user"),
           {
             headers: {
-              Authorization: "Bearer " + localStorage.getItem("token"),
+              Authorization: 'Bearer ' + localStorage.getItem('token'),
             },
           }
         );
-        console.log("response: ", response);
+        console.log('response: ', response);
       } catch (err) {
         console.log(err);
       }
@@ -54,28 +53,28 @@ export default function SearchResult({ teacherList }) {
     <Column>
       {list ? (
         <>
-          <Line className={"filterResult"}>
+          <Line className={'filterResult'}>
             <label>Order By: </label>
             <select
-              name="filterInput"
-              id="filterInput"
+              name='filterInput'
+              id='filterInput'
               onChange={(e) => {
                 setFilter(e.target.value);
               }}
               defaultValue={{ filter }}
             >
-              <option value="Raiting">Raiting</option>
-              <option value="Price">Price</option>
+              <option value='Raiting'>Raiting</option>
+              <option value='Price'>Price</option>
             </select>
           </Line>
-          <Line className="searchResultContainer">
+          <Line className='searchResultContainer'>
             {list.map((teacher, index) => {
               return <TeacherCard teacher={teacher} key={index} />;
             })}
           </Line>
         </>
       ) : (
-        "No teachers to show"
+        'No teachers to show'
       )}
     </Column>
   );
