98a8076dff4f0c6a23c23766c1f740bbb415277b
May Aviram <108925345+MayAviram@users.noreply.github.com>
2023-03-29 18:17:13 +0300

    tabs bugfix

--- music-lessons/src/Views/HomePage.jsx
+++ music-lessons/src/Views/HomePage.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Homepage() {
-  return <div className="homepage">Homepage</div>;
+  return (
+    <div className="homepage">
+      <h1>Start your music journey today</h1>
+    </div>
+  );
 }
